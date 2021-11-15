14cbe93c7716b618479df4326cf7bb2c0dc5cb34
jf dh <jf@opencollective.brussels>
2021-11-15 16:21:59 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [ :index, :show, :edit, :update ] do
+    resources :bookings, only: [ :new, :create ]
+  end
+
+resources :bookings, only: :destroy do
+  resources :reviews, only: [ :new, :create]
+  end
+
+GET 'my_bookings' => 'pages#my_bookings'
+
 end
