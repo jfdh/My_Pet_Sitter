b1d0c8c1b378c9d38b68ac0214f7a6d77969b2fa
jf dh <jf@opencollective.brussels>
2021-11-15 15:25:11 +0100

    first model

---
+++ db/migrate/20211115142412_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.boolean :pet_sitter
+      t.integer :price
+      t.text :review_id
+
+      t.timestamps
+    end
+  end
+end
