a54cc7c7429b28a384afdead813def98aa3c5569
albane orts <albaneorts@gmail.com>
2021-11-18 15:33:12 +0100

    booking-form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,14 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import 'flatpickr/dist/flatpickr.min.css'
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initFlatpickr } from "../plugins/flatpickr";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initFlatpickr();
 });
